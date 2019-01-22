9d02e3ca202c9423a761148bc85612b339866c53
SHENGYUKing <13026369123@163.com>
2019-01-23 00:59:52 +0800

    first and last

---
+++ MyLove.py
@@ -0,0 +1,28 @@
+# -*- coding:utf-8 -*-
+
+# 版权所有 (C) 2019.1.22 金盛羽&张彬嫚。保留所有权利。
+# Copyright 2019.1.22 Shengyu Jin & Binman Zhung. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# From 羽 To 嫚儿
+
+# When I was alive
+while Mylife:
+    # In the future with you
+    if Future with You:
+        # I will always be with you
+        I.will(Always).beWith(You)
+    else:
+        # My heart and life will be broken
+        break
